cc5452d6d84b02d7a297854e31535a9b63f467a8
BDOTimer <41667659+BDOTimer@users.noreply.github.com>
2025-05-11 09:12:16 +0300

    +

--- VS-2022/Visualizator/sources/debug.h
+++ VS-2022/Visualizator/sources/debug.h
@@ -4,6 +4,8 @@
 ///-----------------------------------------------------------------------------
 /// ...
 ///----------------------------------------------------------------------------:
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <functional>
 #include <filesystem> /// C++17
 #include <algorithm>
